bd415fe48943519672dcccc162a08c3e318ca96b
Soxoj <soxoj@protonmail.com>
2024-11-16 17:53:05 +0100

    Param fixes

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -86,7 +86,7 @@ module ExternalPosts
     end
 
     def fetch_content_from_url(url)
-      html = HTTParty.get(url).body
+      html = HTTParty.get(url, headers: { 'User-Agent' => 'SoxojPreviewBot' }).body
       parsed_html = Nokogiri::HTML(html)
 
       title = parsed_html.at('head title')&.text.strip || ''
